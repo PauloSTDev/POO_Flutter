25ca01f5c72e8122825ee1410c0ca3b5cad27de2
paulo.santosDell <afonso.mea@gmail.com>
2022-10-27 12:09:56 -0300

    Adiciona navegação com IconButton

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:poo_flutter/screens/home_screen.dart';
 
 class CategoriesScreen extends StatefulWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
@@ -12,6 +13,13 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white,),
+          onPressed: () {
+            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const HomeScreen()));
+          },
+        ),
+        elevation: 0.0,
         title: const Text("Welcome to Category Screen"),
       ),
     );
