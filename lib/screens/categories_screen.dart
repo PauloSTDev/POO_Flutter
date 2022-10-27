1de6a346b85afbf493fb7dce10abb2c6b2dd76eb
paulo.santosDell <afonso.mea@gmail.com>
2022-10-27 17:19:49 -0300

    Adiciona floatingActionButton com showAlert

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,18 +9,34 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
+
+  _showFormInDialog(BuildContext context) {
+    return showDialog(context: context, builder: (param) {
+      return const AlertDialog(title: Text("Category form"),);
+    }, barrierDismissible: true);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.white,),
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.white,
+          ),
           onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) => const HomeScreen()));
+            Navigator.of(context).push(
+                MaterialPageRoute(builder: (context) => const HomeScreen()));
           },
         ),
         elevation: 0.0,
         title: const Text("Welcome to Category Screen"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _showFormInDialog(context);
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
