eb57ddb05963b8a73043139c99cf96887e6adef2
paulo.santosDell <afonso.mea@gmail.com>
2022-10-27 11:59:44 -0300

    Altera cor principal do App

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -7,6 +7,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.red
+      ),
       home: HomeScreen(),
     );
   }
