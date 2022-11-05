17de702b2eb671f06be996bb88ee65144714b44c
Paulo ST Dell <afonso.mea@gmail.com>
2022-11-05 17:32:18 -0300

    Cria category_service.dart e adiciona serviços

---
+++ lib/repositories/category_service.dart
@@ -0,0 +1,43 @@
+import 'package:poo_flutter/models/category.dart';
+import 'package:poo_flutter/repositories/connection_db.dart';
+
+class CategoryService {
+
+  Future<int> save(Category category) {
+    return connectionDB().then((db) {
+      final Map<String, dynamic> categoryMap = {
+        'id': category.id,
+        'name' : category.name,
+        'description' : category.description
+      };
+
+      print(categoryMap);
+      return db.insert("categories", categoryMap);
+    });
+  }
+
+  Future<List<Category>> findAll() {
+    return connectionDB().then((db) {
+      return db.query('categories').then((maps) {
+        final List<Category> categories = [];
+        for (Map<String, dynamic> map in maps) {
+          final Category category = Category(
+            map['id'],
+            map['name'],
+            map['description'],
+          );
+          categories.add(category);
+        }
+        return categories;
+      });
+    });
+  }
+
+
+  Future<int> deleteCategory(Category category) async{
+    return connectionDB().then((db) {
+      return db.delete("categories", where: "id = ${category.id}");
+    });
+  }
+
+}
