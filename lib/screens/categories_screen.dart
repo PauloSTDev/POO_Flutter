fb128a83b61512969ad48ef81f950b2156241de3
Paulo ST Dell <afonso.mea@gmail.com>
2022-11-08 13:23:48 -0300

    Adiciona screen de edit e setStates

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:poo_flutter/models/category.dart';
 import 'package:poo_flutter/repositories/category_service.dart';
+import 'package:poo_flutter/screens/category_edit_screen.dart';
 import 'package:poo_flutter/screens/home_screen.dart';
 
 class CategoriesScreen extends StatefulWidget {
@@ -14,55 +15,57 @@ class CategoriesScreen extends StatefulWidget {
 }
 
 class _CategoriesScreenState extends State<CategoriesScreen> {
-
   final categoryNameController = TextEditingController();
   final categoryDescriptionController = TextEditingController();
 
   showFormInDialog(BuildContext context) {
-    return showDialog(context: context, builder: (param) {
-      return AlertDialog(
-        actions: [
-          TextButton(
-              onPressed: () async {
-                final category = Category(Random().nextInt(100), categoryNameController.text, categoryDescriptionController.text);
-                print(category);
-                CategoryService().save(category);
-                Navigator.of(context).pop(context);
-                setState(() {
-
-                });
-              },
-              child: const Text("Save")),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(context);
-              },
-              child: const Text("Cancel")),
-
-        ],
-        title: const Text("Category form"),
-        content: SingleChildScrollView(
-          child: Column(
-            children: [
-              TextField(
-                controller: categoryNameController,
-                decoration: const InputDecoration(
-                  labelText: "Category name",
-                  hintText: "Write Category name",
-                ),
-              ),
-              TextField(
-                controller: categoryDescriptionController,
-                decoration: const InputDecoration(
-                  labelText: "Category description",
-                  hintText: "Write Category description",
-                ),
-              ),
+    return showDialog(
+        context: context,
+        builder: (param) {
+          return AlertDialog(
+            actions: [
+              TextButton(
+                  onPressed: () async {
+                    final category = Category(
+                        Random().nextInt(100),
+                        categoryNameController.text,
+                        categoryDescriptionController.text);
+                    print(category);
+                    CategoryService().save(category);
+                    Navigator.of(context).pop(context);
+                    setState(() {});
+                  },
+                  child: const Text("Save")),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(context);
+                  },
+                  child: const Text("Cancel")),
             ],
-          ),
-        ),
-      );
-    }, barrierDismissible: true);
+            title: const Text("Category form"),
+            content: SingleChildScrollView(
+              child: Column(
+                children: [
+                  TextField(
+                    controller: categoryNameController,
+                    decoration: const InputDecoration(
+                      labelText: "Category name",
+                      hintText: "Write Category name",
+                    ),
+                  ),
+                  TextField(
+                    controller: categoryDescriptionController,
+                    decoration: const InputDecoration(
+                      labelText: "Category description",
+                      hintText: "Write Category description",
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+        barrierDismissible: true);
   }
 
   @override
@@ -91,7 +94,62 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
             itemBuilder: (context, index) {
               if (categories.isNotEmpty) {
                 final Category category = categories[index];
-                return _CategoryItem(category, category.name, category.description);
+                return Card(
+                  child: ListTile(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => CategoryEditScreen(
+                              category: category,
+                              name: category.name,
+                              description: category.description
+                          ),
+                        ),
+                      ).then((value) => setState((){print("Recarregando");}));
+                    },
+                    trailing: IconButton(
+                        onPressed: () {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text("Confirm the delete?"),
+                                  actions: [
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop(context);
+                                      },
+                                      child: const Text("Cancel"),
+                                    ),
+                                    ElevatedButton(
+                                      onPressed: () {
+                                        CategoryService()
+                                            .deleteCategory(category).then((value) => setState(() {
+                                          Navigator.of(context).pop(context);
+                                        }));
+                                      },
+                                      child: const Text("Confirm"),
+                                    ),
+                                  ],
+                                );
+                              });
+                        },
+                        icon: const Icon(Icons.delete)),
+                    title: Text(
+                      category.name,
+                      style: const TextStyle(
+                        fontSize: 24.0,
+                      ),
+                    ),
+                    subtitle: Text(
+                      category.description.toString(),
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                );
               }
               return const Center(
                 child: Text("Erro desconhecido"),
@@ -106,106 +164,6 @@ class _CategoriesScreenState extends State<CategoriesScreen> {
           showFormInDialog(context);
         },
         child: const Icon(Icons.add),
-      ),
-    );
-  }
-}
-
-
-class _CategoryItem extends StatelessWidget {
-  final Category category;
-  final String name;
-  final String description;
-  const _CategoryItem(this.category, this.name, this.description);
-
-  @override
-  Widget build(BuildContext context) {
-
-    final categoryNameController = TextEditingController();
-    final categoryDescriptionController = TextEditingController();
-
-    return Card(
-      child: ListTile(
-        onLongPress: () {
-          showDialog(context: context, builder: (param) {
-            return AlertDialog(
-              actions: [
-                TextButton(
-                    onPressed: () async {
-                      final updatedCategory = Category(category.id, categoryNameController.text, categoryDescriptionController.text);
-                      await CategoryService().update(updatedCategory)
-                      .then((value) => Navigator.of(context).pop(context));
-
-                    },
-                    child: const Text("Save")),
-                TextButton(
-                    onPressed: () {
-                      Navigator.of(context).pop(context);
-                    },
-                    child: const Text("Cancel")),
-
-              ],
-              title: const Text("Category form"),
-              content: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    TextField(
-                      controller: categoryNameController,
-                      decoration: InputDecoration(
-                        labelText: "Category name",
-                        hintText: name,
-                      ),
-                    ),
-                    TextField(
-                      controller: categoryDescriptionController,
-                      decoration: InputDecoration(
-                        labelText: "Category description",
-                        hintText: description,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            );
-          }, barrierDismissible: true);
-        },
-        trailing: IconButton(
-            onPressed: () {
-              showDialog(context: context, builder: (BuildContext context) {
-                return AlertDialog(
-                  title: const Text("Confirm the delete?"),
-                  actions: [
-                    ElevatedButton(
-                      onPressed: () {
-                        Navigator.of(context).pop(context);
-                      },
-                      child: const Text("Cancel"),
-                    ),
-                    ElevatedButton(
-                      onPressed: () {
-                        CategoryService().deleteCategory(category);
-                        Navigator.of(context).pop(context);
-                      },
-                      child: const Text("Confirm"),
-                    ),
-                  ],
-                );
-              });
-
-            },
-            icon: const Icon(Icons.delete)),
-        title: Text(
-          category.name,
-          style: const TextStyle(
-            fontSize: 24.0,
-          ),
-        ),
-        subtitle: Text(
-          category.description.toString(),
-          style: const TextStyle(
-            fontSize: 16.0,
-          ),
-        ),
       ),
     );
   }
