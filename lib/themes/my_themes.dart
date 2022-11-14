de4b0b6ad9550982f74dd242a3332d8247c79ece
Paulo ST Dell <afonso.mea@gmail.com>
2022-11-14 11:19:15 -0300

    Cria e separa os temas da aplicação

---
+++ lib/themes/my_themes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:poo_flutter/themes/theme_colors.dart';
+
+ThemeData MyTheme = ThemeData(
+  primarySwatch: ThemeColors.primaryColor,
+  primaryColor: ThemeColors.primaryColor,
+);
---
+++ lib/themes/theme_colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const MaterialColor primaryColor = Colors.purple;
+  static const MaterialColor drawerHeaderColor = Colors.purple;
+}
