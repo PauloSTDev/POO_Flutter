1b112f207a652cfab2d95a4d057d318203d19adf
Paulo ST Dell <afonso.mea@gmail.com>
2022-11-16 22:22:49 -0300

    Cria modelo de task

---
+++ lib/models/task.dart
@@ -0,0 +1,17 @@
+class Task {
+  int id;
+  String title;
+  String description;
+  String category;
+  String todoDate;
+  int isFinished;
+
+  Task(
+    this.id,
+    this.title,
+    this.description,
+    this.category,
+    this.todoDate,
+    this.isFinished,
+  );
+}
