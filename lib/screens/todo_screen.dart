de665c1ca99fd7a54f0081f2a99a5e34193b56e9
Paulo ST Dell <afonso.mea@gmail.com>
2022-11-16 21:39:17 -0300

    Cria botã de calendário pra preenchimento de datas

--- lib/screens/todo_screen.dart
+++ lib/screens/todo_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:poo_flutter/components/colors_dot.dart';
 import 'package:poo_flutter/database/dao/category_service.dart';
 import 'package:poo_flutter/themes/theme_colors.dart';
+import 'package:intl/intl.dart';
 
 class TodoScreen extends StatefulWidget {
   const TodoScreen({Key? key}) : super(key: key);
@@ -23,9 +24,9 @@ class _TodoScreenState extends State<TodoScreen> {
     _loadCategories();
   }
   _loadCategories() async {
-    final _categoryService = CategoryService();
-    final categories = await _categoryService.findAll();
-    categories.forEach((category) {
+    final categoryService = CategoryService();
+    final categories = await categoryService.findAll();
+    for (var category in categories) {
       setState(() {
         _categories.add(
           DropdownMenuItem(
@@ -40,7 +41,19 @@ class _TodoScreenState extends State<TodoScreen> {
           ),
         );
       });
-    });
+    }
+  }
+
+  DateTime _date = DateTime.now();
+
+  _selectTodoDate(BuildContext context) async {
+    final pickedDate = await showDatePicker(context: context, initialDate: _date, firstDate: DateTime(2000), lastDate: DateTime(2099));
+    if (pickedDate != null){
+      _date = pickedDate;
+      setState(() {
+        _todoDate.text = DateFormat("yyyy-MM-dd").format(pickedDate);
+      });
+    }
   }
 
   @override
@@ -66,16 +79,20 @@ class _TodoScreenState extends State<TodoScreen> {
             ),
             TextField(
               controller: _todoDate,
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                   hintText: "YY-MM-DD",
                   labelText: "YY-MM-DD",
-                  prefixIcon: Icon(Icons.calendar_today)),
+                  prefixIcon: InkWell(
+                    onTap: () => _selectTodoDate(context),
+                      child: const Icon(Icons.calendar_today))),
             ),
             DropdownButtonFormField(
               value: _selectedValue,
               items: _categories,
               hint: const Text("Select one category"),
-              onChanged: (value) {},
+              onChanged: (value) {
+                _selectedValue = value;
+              },
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
