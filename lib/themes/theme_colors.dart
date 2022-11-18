617e88a970354f0e6a61d561cd5d37b9794ae8db
Paulo ST Dell <afonso.mea@gmail.com>
2022-11-18 10:38:50 -0300

    Altera nome do colorDot

--- lib/themes/theme_colors.dart
+++ lib/themes/theme_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class ThemeColors {
   static const MaterialColor primaryColor = Colors.purple;
   static const MaterialColor drawerHeaderColor = Colors.purple;
-  static const colorDotCategory = {
+  static const colorDotCategoryAndTask = {
     "Platina 3" : Color.fromRGBO(98, 167, 248, 1.0),
     "Ouro 3" : Color.fromRGBO(254, 241, 85, 1.0),
     "Bronze 3" : Color.fromRGBO(191, 142, 0, 1.0),
