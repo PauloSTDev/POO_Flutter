92b3729daec3e27ef4bd71d55558d0cef0009d89
Paulo ST Dell <afonso.mea@gmail.com>
2022-11-17 22:09:04 -0300

    Adiciona implementação de findAll na home_screen.dart

--- lib/database/task_database/dao/task_service.dart
+++ lib/database/task_database/dao/task_service.dart
@@ -3,6 +3,7 @@ import 'package:poo_flutter/models/task.dart';
 
 class TaskService {
   static const String table = "tasks";
+
   Future<int> save(Task task) {
     return taskConnectionDB().then((db) {
       final Map<String, dynamic> taskMap = {
@@ -14,6 +15,27 @@ class TaskService {
         'isFinished': task.isFinished
       };
       return db.insert(table, taskMap);
+    });
+  }
+
+  Future<List<Task>> findAll() {
+    return taskConnectionDB().then((db) {
+      return db.query(table).then((maps) {
+        final List<Task> tasks = [];
+        for (Map<String, dynamic> map in maps) {
+          final Task task = Task(
+            map["id"],
+            map["title"],
+            map["description"],
+            map["category"],
+            map["todoDate"],
+            map["isFinished"],
+          );
+          tasks.add(task);
+        }
+        return tasks;
+
+      });
     });
   }
 }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:poo_flutter/database/task_database/dao/task_service.dart';
+import 'package:poo_flutter/models/task.dart';
 import 'package:poo_flutter/widgets/drawer.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -16,6 +18,32 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text("POO Flutter"),
       ),
       drawer: const DrawerNavigation(),
+      body: FutureBuilder<List<Task>>(
+        initialData: const [],
+        future: TaskService().findAll(),
+        builder: (context, snapshot){
+          List<Task>? tasks = snapshot.data;
+          return ListView.builder(
+            itemCount: tasks?.length,
+              itemBuilder: (context, index){
+                if (tasks != null){
+                  final Task task = tasks[index];
+                  return Card(
+                    child: ListTile(
+                      onTap: () {},
+                      leading: const Icon(Icons.connecting_airports_sharp),
+                      title: Text(task.title),
+                      subtitle: Text(task.description),
+                    ),
+                  );
+                }
+                else {
+                  return const CircularProgressIndicator();
+                }
+              },
+          );
+        },
+      ),
       floatingActionButton:
           FloatingActionButton(onPressed: () {}, child: const Icon(Icons.add)),
     );
